8b13ffad7da65075af4b2d00155e6deb5946a727
pauline <16977973+mpgxvii@users.noreply.github.com>
2019-04-26 14:56:15 +0100

    fix import

--- src/android/FirebasePlugin.java
+++ src/android/FirebasePlugin.java
@@ -20,6 +20,7 @@ import com.google.firebase.FirebaseApp;
 import com.google.firebase.analytics.FirebaseAnalytics;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.RemoteMessage;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfig;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigInfo;
 import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings;
